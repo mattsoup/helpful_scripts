a24c3e913f8e76a3afaa463e113bf741db8e768a
Matt Campbell <mcampbell@neb.com>
2019-03-18 12:14:57 -0400

    Adding capability to read gzipped fasta files

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 
+import gzip
 
 def parse_fasta(fasta_file):
 
 	print("Parsing {}".format(fasta_file))
 	fasta_dict = {}
-	fasta = open(fasta_file, "r")
-	for line in fasta:
-		if line.startswith(">"):
-			header = line.strip().split(" ")[0][1:]
-			print(header)
-			fasta_dict[header] = []
-		else:
-			fasta_dict[header].append(line.strip())
-	fasta.close()	
+	if fasta_file.endswith(".gz"):
+		fasta = gzip.open(fasta_file, "rb")
+		for line in fasta:
+			line = str(line, "utf-8")
+			if line.startswith(">"):
+				header = line.strip().split(" ")[0][1:]
+				print(header)
+				fasta_dict[header] = []
+			else:
+				fasta_dict[header].append(line.strip())
+		fasta.close()	
+	else:
+		fasta = open(fasta_file, "r")
+		for line in fasta:
+			if line.startswith(">"):
+				header = line.strip().split(" ")[0][1:]
+				print(header)
+				fasta_dict[header] = []
+			else:
+				fasta_dict[header].append(line.strip())
+		fasta.close()	
 
 	for header in fasta_dict:
 		fasta_dict[header] = "".join(fasta_dict[header])
